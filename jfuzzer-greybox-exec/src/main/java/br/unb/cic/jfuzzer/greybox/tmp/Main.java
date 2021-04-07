e85263698559dbfa82b90f0fa82c4eeae28e41ee
luisamaralh <luis.amaralh@gmail.com>
2021-04-07 18:05:59 -0300

    First implementation of greybox-fuzzer

--- jfuzzer-greybox-exec/src/main/java/br/unb/cic/jfuzzer/greybox/tmp/Main.java
+++ jfuzzer-greybox-exec/src/main/java/br/unb/cic/jfuzzer/greybox/tmp/Main.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import br.unb.cic.jfuzzer.FuzzerConfig;
 import br.unb.cic.jfuzzer.api.RunnerResult;
+import br.unb.cic.jfuzzer.fuzzer.StringFuzzer;
 import br.unb.cic.jfuzzer.greybox.GreyBoxFuzzer;
 import br.unb.cic.jfuzzer.util.Range;
 import br.unb.cic.jfuzzer.util.observer.JFuzzerObservable;
@@ -15,34 +16,39 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Beginning execution ...");
 
-//        System.err.println(NumberUtils.createLong("0x08000000000000000000"));
-//        System.err.println(NumberUtils.createLong("0x080000000"));
-//        System.err.println(NumberUtils.createLong("0x72321741"));        
+        // System.err.println(NumberUtils.createLong("0x08000000000000000000"));
+        // System.err.println(NumberUtils.createLong("0x080000000"));
+        // System.err.println(NumberUtils.createLong("0x72321741"));
 
         // commons codec
-        GreyBoxFuzzer fuzzer = new GreyBoxFuzzer(new Range<>(30, 50), FuzzerConfig.getDefaultRandom(), alphabet());
-      CommonsCodecRunner runner = new CommonsCodecRunner();
+        // GreyBoxFuzzer fuzzer = new GreyBoxFuzzer(new Range<>(30, 50),
+        // FuzzerConfig.getDefaultRandom(), alphabet());
+        GreyBoxFuzzer fuzzer = new GreyBoxFuzzer(new Range<>(99, 101), FuzzerConfig.getDefaultRandom(),
+                StringFuzzer.ALPHANUMERIC);
 
-        //commons lang
-//        GreyBoxFuzzer fuzzer = new GreyBoxFuzzer(new Range<>(5, 10), FuzzerConfig.getDefaultRandom(), "x0123456789");
-//        CommonsLangNumberUtilsRunner runner = new CommonsLangNumberUtilsRunner();
-//
+        CommonsCodecRunner runner = new CommonsCodecRunner();
+
+        // commons lang
+        // GreyBoxFuzzer fuzzer = new GreyBoxFuzzer(new Range<>(5, 10),
+        // FuzzerConfig.getDefaultRandom(), "x0123456789");
+        // CommonsLangNumberUtilsRunner runner = new CommonsLangNumberUtilsRunner();
+        //
         JFuzzerObservable.addObserver(fuzzer);
 
-        List<RunnerResult<String>> results = fuzzer.run(runner, 5);
+        List<RunnerResult<String>> results = fuzzer.run(runner, 50000);
 
-        System.out.println("\n ********* RESULTS **********");
-        results.forEach(System.out::println);
+        // System.out.println("\n ********* RESULTS **********");
+        // results.forEach(System.out::println);
 
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//        CoverageUtil.printLines();
-//        CoverageUtil.printMethods();
-//        CoverageUtil.printBranches();
+        // try {
+        // Thread.sleep(1000);
+        // } catch (InterruptedException e) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // }
+        // CoverageUtil.printLines();
+        // CoverageUtil.printMethods();
+        // CoverageUtil.printBranches();
     }
 
     private static String alphabet() {
--- jfuzzer-greybox/src/main/java/br/unb/cic/jfuzzer/greybox/GreyBoxFuzzer.java
+++ jfuzzer-greybox/src/main/java/br/unb/cic/jfuzzer/greybox/GreyBoxFuzzer.java
@@ -1,9 +1,15 @@
 package br.unb.cic.jfuzzer.greybox;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Random;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 import br.unb.cic.jfuzzer.api.Runner;
 import br.unb.cic.jfuzzer.api.RunnerResult;
@@ -15,39 +21,48 @@ import br.unb.cic.jfuzzer.util.observer.JFuzzerObserver;
 
 public class GreyBoxFuzzer extends StringFuzzer implements JFuzzerObserver {
 
-    //dummy implementation for now
-    
+    // dummy implementation for now
+
     private List<Event> events = new LinkedList<>();
-    
+
     public GreyBoxFuzzer(Range<Integer> range, Random random, String symbols) {
         super(range, random, symbols);
     }
-    
+
     @Override
-    public List<RunnerResult<String>> run(Runner runner, int trials){        
+    public List<RunnerResult<String>> run(Runner runner, int trials) {
         List<RunnerResult<String>> outcomes = new ArrayList<>(trials);
+        HashMap<String, Float> seedsCoverage = new HashMap<String, Float>();
+
         for (int i = 0; i < trials; i++) {
             events = new LinkedList<>();
             RunnerResult<String> result = runner.run(fuzz());
             outcomes.add(result);
-            
-            showEvents();
-            
+
+            // showEvents();
+
             CoverageSummary summary = new CoverageSummary(events);
             float lineCoverage = summary.getLineCoveragePercentage();
-            
-            System.out.println("************** SUMMARY ********** ");
-            System.out.println(summary);
-            
-            System.err.printf("LINE_COVERAGE_PERCENTAGE: %5.2f %% %n",lineCoverage);
+            seedsCoverage.put(result.getInput(), lineCoverage);
+
+            // System.out.println("************** SUMMARY ********** ");
+            // System.out.println(summary);
+
+            // System.err.printf("LINE_COVERAGE_PERCENTAGE: %5.2f %% %n",lineCoverage);
         }
+        // System.out.println(seedsCoverage.values());
+        Float maxCoverage = (Collections.max(seedsCoverage.values()));
+        Map<String, Float> bestSeed = seedsCoverage.entrySet().stream()
+                .filter(map -> maxCoverage.equals(map.getValue()))
+                .collect(Collectors.toMap(map -> map.getKey(), map -> map.getValue()));
+        System.out.println(bestSeed);
         return outcomes;
     }
-    
+
     private void showEvents() {
-        events.forEach(e -> System.out.println(">>> "+ e));
+        events.forEach(e -> System.out.println(">>> " + e));
     }
-    
+
     @Override
     public void updateEvent(Event event) {
         events.add(event);
